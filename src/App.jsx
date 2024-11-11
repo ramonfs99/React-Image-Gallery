5494a85317548ed501275cf9bc103cbbee3157f8
Ramón Fuertes <fsramon3@gmail.com>
2024-11-11 12:02:50 +0100

    added basic project file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { LayoutComponent } from "./components/LayoutComponent";
+import { HomePage } from "./pages/HomePage";
+import { MyPhotosPage } from "./pages/MyPhotosPage";
 
 function App() {
-  const [images, setImages] = useState(0)
+  const [images, setImages] = useState([]);
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LayoutComponent />}>
+            <Route element={<HomePage />}></Route>
+            <Route element={<MyPhotosPage/>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <div>
         <h1>Hello World</h1>
+        {images.map((image) => (
+          <img src={image.url} alt="" />
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LayoutComponent.jsx
@@ -0,0 +1,5 @@
+export const LayoutComponent = () => {
+    return <>
+    
+    </>
+}
